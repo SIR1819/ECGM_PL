ddd4b5488fc16f5383961b193c4e9e854b2ab474
pedromoreira-estg <pmoreira.estg@gmail.com>
2018-10-02 11:50:55 +0100

    classe geradora

---
+++ EUROMILHOES/CGeraChave.php
@@ -0,0 +1,43 @@
+<?php
+
+// classe
+class GeraChave {
+    // propriedades
+    public $estrelas = array();
+    public $numeros = array();
+
+    // metodos
+    
+    // construtor
+    public function __construct () {
+        $this->estrelas = $this->gera(1,12,2);
+        $this->numeros  = $this->gera(1,50,5);
+    }
+
+    private function gera($min, $max, $num) {
+        $chave = array();
+        while (sizeof($chave) < $num) {
+            $novo = rand($min,$max);
+            if (!in_array($novo,$chave)) {
+                array_push($chave, $novo);
+            }
+        } 
+        sort($chave);
+        return $chave;
+    }
+}
+
+
+// instanciar um objeto de uma classe
+$chave = new GeraChave();
+
+
+// --- TESTES ----
+// var_dump($chave);
+// var_dump($chave->numeros);
+
+// $chave->numeros[] = 100;
+
+// var_dump($chave->numeros);
+
+?>
